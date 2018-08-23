36612e328aaeb0771037678b29ded5a212d8bf12
Olawale Ajibare <olawale.ajibare@gmail.com>
2018-08-23 22:52:26 +0100

    wsgi changes

--- mysite1/mysite1/wsgi.py
+++ mysite1/mysite1/wsgi.py
@@ -9,6 +9,15 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+path = '/home/hubprojects/mysite1'
+if path not in  sys.path:
+	sys.path.append(path)
+
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'projectname.settings'
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite1.settings")
